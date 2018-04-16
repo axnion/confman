fd7b7d599601d6efa0e4c89ebe32cb0a6f2aa850
Axel Nilsson <an222yp@student.lnu.se>
2018-04-17 01:45:13 +0200

    Fixed my fonts

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,3 +3,4 @@ apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
+git clone https://github.com/pigmonkey/ansible-aur.git library/ansible-aur
