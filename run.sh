e8e43c0aa83453e5e69e15953751e41c1c2bf8eb
axnion <axelnilsson1994@gmail.com>
2019-04-06 20:06:30 +0200

    Updated setup with Arch support, also updated vim and tmux roles

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+DISTRO="$(cat /etc/*-release | grep ID_LIKE | awk -F '"' '{print $2}')"
 
 # Install Ansible if it's not installed
 if ! [ -x "$(command -v ansible --version)" ]; then
-  add-apt-repository universe
-  apt-get -y install software-properties-common
-  apt-add-repository -y ppa:ansible/ansible
-  apt-get update
-  apt-get -y install ansible
+  if [ "$DISTRO" = "ubuntu" ]; then
+    echo "Installing Ansible for Ubuntu"
+    add-apt-repository universe
+    apt-get -y install software-properties-common
+    apt-add-repository -y ppa:ansible/ansible
+    apt-get update
+    apt-get -y install ansible
+  elif [ "$DISTRO" = "arch" ]; then
+    echo "Installing Ansible for Arch"
+    pacman -S ansible --noconfirm
+    yay -Ss ansible-aur-git
+  fi
 fi
 
 # Run playbook locally
-ansible-playbook --connection=local --ask-become-pass setup.yml 
+ansible-playbook --connection=local --ask-become-pass setup.yml
+
